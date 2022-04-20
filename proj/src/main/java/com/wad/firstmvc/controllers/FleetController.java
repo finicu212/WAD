42e0c8b06fc67f54b38c6df50299ba4acd71d945
finicu212 <smurfdeplastic@gmail.com>
2022-04-20 17:28:57 +0300

    fleet.html improvements

--- proj/src/main/java/com/wad/firstmvc/controllers/FleetController.java
+++ proj/src/main/java/com/wad/firstmvc/controllers/FleetController.java
@@ -3,12 +3,19 @@ package com.wad.firstmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/fleet")
 public class FleetController {
     @GetMapping
+    public String fleet() {
+        //returns the view
+        return "fleet";
+    }
+
+    @PostMapping("/home")
     public String home() {
         //returns the view
         return "fleet";
