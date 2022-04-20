d56f216bdc5053cbb4037ee71b93b7a82f4f94d2
finicu212 <smurfdeplastic@gmail.com>
2022-04-20 17:03:46 +0300

    admin fleet dashboard

--- proj/src/main/java/com/wad/firstmvc/controllers/FleetController.java
+++ proj/src/main/java/com/wad/firstmvc/controllers/FleetController.java
@@ -3,20 +3,13 @@ package com.wad.firstmvc.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class HomeController {
+@RequestMapping("/fleet")
+public class FleetController {
     @GetMapping
     public String home() {
-        //returns the view
-        return "index";
-    }
-
-    @PostMapping
-    public String showFleetPage() {
         //returns the view
         return "fleet";
     }
