2419654d72e2f2ba58a5d96bdb2ab81aed931b74
finicu212 <smurfdeplastic@gmail.com>
2022-05-30 22:24:02 +0300

    forms newvehicle

--- proj/src/main/java/com/wad/firstmvc/controllers/FleetController.java
+++ proj/src/main/java/com/wad/firstmvc/controllers/FleetController.java
@@ -2,6 +2,7 @@ package com.wad.firstmvc.controllers;
 
 import com.wad.firstmvc.domain.Appointment;
 import com.wad.firstmvc.domain.MaintenanceItem;
+import com.wad.firstmvc.domain.Vehicle;
 import com.wad.firstmvc.repositories.AppointmentRepository;
 import com.wad.firstmvc.repositories.VehicleRepository;
 import com.wad.firstmvc.services.MaintenanceItemService;
@@ -9,10 +10,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 @Controller
 @RequestMapping("/fleet")
@@ -24,11 +27,26 @@ public class FleetController {
     @Autowired
     VehicleRepository vehicleRepository;
 
-
     @GetMapping
     public String dashboard(Model model) {
         model.addAttribute("vehicles", vehicleRepository.findAll());
         System.out.println(model.getAttribute("vehicles"));
         return "fleet";
+    }
+
+    @GetMapping("/new")
+    public String showVehiclesForm(Model model) {
+        model.addAttribute("vehicle", new Vehicle());
+
+        return "newvehicle";
+    }
+
+    @PostMapping("/new")
+    public String addVehicle(Vehicle vehicle) {
+        if (vehicle.getId() == null)
+            vehicle.setId(new Random().nextLong());
+        vehicleRepository.save(vehicle);
+
+        return "redirect:/fleet";
     }
 }
